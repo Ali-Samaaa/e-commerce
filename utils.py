885be536f25eb77c3c0bc5d118278e8a2d121fbc
Ali-Samaaa <139652020+Ali-Samaaa@users.noreply.github.com>
2024-09-18 11:59:00 +0330

    send sms to user successfully

--- utils.py
+++ utils.py
@@ -1,3 +1,17 @@
+from kavenegar import *
+
 
 def send_opt_code(phone_number, code):
-    pass
+    try:
+        api = KavenegarAPI('4145385A7A3661704746346D73475630456151693145696F4B4A587148774259464733536E6C377743776F3D')
+        params = {
+            'sender': '',
+            'receptor': phone_number,
+            'message': f'کد شما: {code}'
+        }
+        response = api.sms_send(params)
+        print(response)
+    except APIException as e:
+        print(e)
+    except HTTPException as e:
+        print(e)

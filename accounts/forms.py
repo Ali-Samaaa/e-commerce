91a62e1e8ec8dac46d65ec0773877e2ccd52500f
Ali-Samaaa <139652020+Ali-Samaaa@users.noreply.github.com>
2024-09-18 22:38:17 +0330

    user login not done

--- accounts/forms.py
+++ accounts/forms.py
@@ -58,3 +58,8 @@ class UserRegistrationForm(forms.Form):
 
 class VerifyCodeForm(forms.Form):
     code = forms.IntegerField()
+
+
+class UserLoginFrom(forms.Form):
+    phone_number = forms.CharField(max_length=11)
+    password = forms.CharField(widget=forms.PasswordInput)
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,6 @@
-import datetime
-
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm, VerifyCodeForm
+from .forms import UserRegistrationForm, VerifyCodeForm, UserLoginFrom
 from utils import send_opt_code
 import random
 from .models import OptCode, User
@@ -67,3 +65,63 @@ class UserRegisterVerifyCodeView(View):
                 messages.error(request, 'this code is wrong.', 'danger')
                 return redirect('accounts:verify_code')
         return redirect('home:home')
+
+
+class UserLoginView(View):
+    form_class = UserLoginFrom
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, 'accounts/login.html', {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            code = random.randint(1000, 9999)
+            # send_opt_code(form.cleaned_data['phone_number'], code=code)
+            OptCode.objects.create(phone_number=form.cleaned_data['phone_number'], code=code)
+            print('B'*90)
+            print(code)
+            request.session['user_login_info'] = {
+                'phone_number': form.cleaned_data['phone_number'],
+                'password': form.cleaned_data['password']
+            }
+            messages.success(request, 'we send you a sms', 'success')
+            return redirect('accounts:verify_code')
+        return render(request, 'accounts/login.html', {'form': form})
+
+
+class UserLoginVerifyCodeView(View):
+    form_class = VerifyCodeForm
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, 'accounts/verify.html', {'form': form})
+
+    def post(self, request):
+        user_session = request.session['user_login_info']
+        code = OptCode.objects.filter(phone_number=user_session['code'])
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            if cd['code'] == code.code:
+                user = code.values('created')
+                user = list(user)
+                expired = user[0]['created'] + timedelta(minutes=2)
+                print('A'*90)
+                print(code.code)
+                if timezone.now() <= expired:
+                    User.objects.create_user(user_session['phone_number'], user_session['email'],
+                                             user_session['full_name'], user_session['password'])
+                    code.delete()
+                    messages.success(request, 'you logged successfully', 'success')
+                    return redirect('home:home')
+                else:
+                    messages.error(request, 'you code is expired, please try again', 'danger')
+                    code.delete()
+                    return redirect('accounts:user_login')
+            else:
+                messages.error(request, 'this code is wrong', 'danger')
+                return redirect('home:verify_code')
+        else:
+            return redirect('home:home')

0a4968bd5025437800661bc76ae7a05f6a651838
Ali-Samaaa <139652020+Ali-Samaaa@users.noreply.github.com>
2024-09-21 17:44:50 +0330

    show product in home page and detail page

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -20,7 +21,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d/')
     description = models.TextField()
-    prince = models.IntegerField()
+    price = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -30,3 +31,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('home:product_detail', args=[self.slug, ])
--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Product
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/home.html')
+        products = Product.objects.filter(available=True)
+        return render(request, 'home/home.html', {'products': products})
+
+
+class ProductDetailView(View):
+    def get(self, request, slug):
+        product = Product.objects.get(slug=slug)
+        return render(request, 'home/detail.html', {'product': product})
